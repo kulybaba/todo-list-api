01628fa39e64db44c96646a556e4f6ade2535bd7
Petro Kulybaba <ahurtep@gmail.com>
2019-01-20 18:19:42 +0200

    Adding and removing attachments

--- src/Controller/Api/AttachmentController.php
+++ src/Controller/Api/AttachmentController.php
@@ -3,10 +3,15 @@
 namespace App\Controller\Api;
 
 use App\Entity\Attachment;
+use App\Entity\Item;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class AttachmentController extends AbstractController
 {
@@ -36,5 +41,50 @@ class AttachmentController extends AbstractController
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         return $this->json($attachment);
+    }
+
+    /**
+     * @Route("/api/todo-lists/items/{id<\d+>}/attachments/create", methods={"POST"})
+     */
+    public function createAction(Request $request, Item $item, SerializerInterface $serializer, ValidatorInterface $validator)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        if (!$request->getContent()) {
+            throw new HttpException('400', 'Bad request');
+        }
+
+        $attachment = $serializer->deserialize($request->getContent(), Attachment::class, JsonEncoder::FORMAT);
+        $attachment->setItem($item);
+
+        if (count($validator->validate($attachment))) {
+            throw new HttpException('400', 'Bad request');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($attachment);
+        $em->flush();
+
+        return $this->json($item);
+    }
+
+    /**
+     * @Route("/api/todo-lists/items/{id<\d+>}/attachments/delete", methods={"DELETE"})
+     */
+    public function deleteAction(Item $item)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($item->getAttachment());
+        $em->flush();
+
+        $item->setAttachment(null);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($item);
+        $em->flush();
+
+        return $this->json($item);
     }
 }
